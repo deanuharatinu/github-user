518d934c97b8e61d56e6e7b1b11cced5b9de4677
deanuharatinu <deanu.alt@gmail.com>
2022-11-07 21:23:49 +0700

    Add new dependencies. Add permission for internet. Add application class.

---
+++ app/src/main/java/com/deanu/githubuser/GithubUserApplication.kt
@@ -0,0 +1,5 @@
+package com.deanu.githubuser
+
+import android.app.Application
+
+class GithubUserApplication : Application()
--- app/src/main/java/com/deanu/githubuser/common/MainActivity.kt
+++ app/src/main/java/com/deanu/githubuser/common/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.deanu.githubuser
+package com.deanu.githubuser.common
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.deanu.githubuser.R
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
